a2045735428039e1723de81e7d5c7216a962a5e4
Yahya Ali <yahyaalia394@gmail.com>
2026-09-04 15:15:10 +0500

    newfilesadd

--- Dashboard/src/utils/axiosInstance.js
+++ Dashboard/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://clothes-project-production.up.railway.app/';
 
 // 1. Axios instance banayein
 const api = axios.create({
